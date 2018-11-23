d669e686324d4f443abf1768a5a0cca194d8691d
Dirk Dobson <dirkvancouver@live.com>
2018-11-23 16:44:49 -0700

    inial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace csIteration1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
